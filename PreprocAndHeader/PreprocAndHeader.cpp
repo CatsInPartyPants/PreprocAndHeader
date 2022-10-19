dd00008af028a35791fb1ed9a0fe75d6a1c5a0d8
CatsInPartyPants <81612454+CatsInPartyPants@users.noreply.github.com>
2022-10-19 23:46:02 +0300

    Добавьте файлы проекта.

---
+++ PreprocAndHeader/PreprocAndHeader.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "function.h"
+
+int main()
+{
+    //task2
+    std::cout << D_MIN(5,10) << std::endl;
+    std::cout << D_MAX(5, 10) << std::endl;
+    std::cout << D_SQR(3) << std::endl;
+    std::cout << D_POW(3,2) << std::endl;
+    std::cout << D_EVEN(11);
+    std::cout << D_ODD(11);
+
+}
---
+++ PreprocAndHeader/function.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+//task2
+#define D_MIN(A,B) ((A) < (B) ? (A) : (B))
+#define D_MAX(A,B) ((A) > (B) ? (A) : (B))
+#define D_SQR(A) ((A)*(A))
+#define D_POW(A,B) pow((A), (B))
+#define D_EVEN(A) ((A) % 2 == 0) ? 1 : 0
+#define D_ODD(A) ((A) % 2 == 1) ? 1 : 0
+#endif // !FUNCTION_H_
+
